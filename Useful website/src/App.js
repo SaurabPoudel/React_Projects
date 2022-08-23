5ff656453bd3bf78c73f37bf798081f2da768f36
Saurab Poudel <69349824+SaurabPoudel@users.noreply.github.com>
2022-08-23 13:31:24 +0545

    Add files via upload

---
+++ Useful website/src/App.js
@@ -0,0 +1,17 @@
+import Button from './Button'
+const App=()=>{
+    return <div>
+        <h1> Useful Website</h1>
+        <h3>Which website do you want to open?</h3>
+       <Button name="Google" link="https://google.com"/>
+       <Button name="Facebook"/>
+       <Button name="Youtube"/>
+       <br></br>
+       <Button name="IPO result"/>
+       <Button name="Github"/>
+       <Button name="fpl"/>
+       
+
+    </div>
+}
+export default App;
